1cc2b884dab511d5b276ada7f9a05cb16cb813d9
m-miler <mmiler30@gmail.com>
2022-04-17 22:18:29 +0200

    update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Real time gender and age detection
+# Gender and Age Detection with Keras and OpenCV
+
+Projekt prostej aplikacji za pomocą której możemy w czasie rzeczywistym lub czytując zdjęcie z dysku odnaleźć twarz oraz przewidzieć płeć i wiek.
+
+Dane do projektu:
+
--- real_time_detection.py
+++ real_time_detection.py
@@ -1,11 +1,14 @@
 import tkinter as tk
 import cv2
 import PIL
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageOps
 from matplotlib import image
 import datetime
 import os
 from tkinter import filedialog
+import numpy as np
+from pathlib import Path
+from tensorflow.keras.models import  load_model
 
 class gad_app(tk.Frame):
     def __init__(self, master=None):
@@ -18,6 +21,9 @@ class gad_app(tk.Frame):
         self.width = self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)
         self.height = self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
         self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+        self.gender_model = load_model('ml_model/gender_model')
+        self.age_model = load_model('ml_model/age_model')
 
         self.main_window()
 
@@ -52,12 +58,11 @@ class gad_app(tk.Frame):
         self.camera_frame = tk.Canvas(self.camera_window, bd=2, relief="groove", width=self.width, height=self.height)
         self.camera_frame.pack(fill='both', padx=10, pady=10)
         
-        
-        snapshot = tk.Button(self.camera_window, text='Take a snapshot', width=50, font=['Times', '8', 'bold'])
-        snapshot.pack(fill='both', padx=10, pady=5, anchor='n')
-        snapshot.bind('<Button>', lambda x: self.take_snapshot())
+        # snapshot = tk.Button(self.camera_window, text='Take a snapshot', width=50, font=['Times', '8', 'bold'])
+        # snapshot.pack(fill='both', padx=10, pady=5, anchor='n')
+        # snapshot.bind('<Button>', lambda x: self.take_snapshot())
 
-        back = tk.Button(self.camera_window, text='Back', width=50, font=['Times', '8', 'bold'])
+        back = tk.Button(self.camera_window, text='Back', width=50, font=['Times', '10', 'bold'])
         back.pack(fill='both', padx=10, pady=5, anchor='n')
         back.bind('<Button>', lambda x: self.main_window(self.camera_window))
 
@@ -76,19 +81,33 @@ class gad_app(tk.Frame):
         self.image_frame = tk.Canvas(self.image_window, bd=2, relief="groove", width=self.width, height=self.height)
         self.image_frame.pack(fill='both', padx=10, pady=10)
         
-        load_image = tk.Button(self.image_window, text='Load image for detection', width=50, font=['Times', '8', 'bold'])  
+        load_image = tk.Button(self.image_window, text='Load image for detection', width=50, font=['Times', '10', 'bold'])  
         load_image.pack(fill='both', padx=10, pady=5, anchor='n')
         load_image.bind('<Button>', lambda x: self.browse_img_file())
         
-        back = tk.Button(self.image_window, text='Back', width=50, font=['Times', '8', 'bold'])
+        back = tk.Button(self.image_window, text='Back', width=50, font=['Times', '10', 'bold'])
         back.pack(fill='both', padx=10, pady=5, anchor='n')
         back.bind('<Button>', lambda x: self.main_window(self.image_window))
 
         self.image_window.pack(fill='both', expand=True)
 
     def draw_found_faces(self, detected, image, color: tuple):
+        gen_encoder = {0: 'Female', 1: 'Male'}
+        age_encoder = {0: '0-2', 1: '4-6', 2: '8-13', 3: '15-20', 4: '25-32', 5: '38-43', 6: '48-53', 7: '60+'}
+                
+        img_to_pred = np.array(cv2.resize(image, (224, 224))).reshape(1, 224, 224, 3)
+        
+        gender_pred = self.gender_model.predict(img_to_pred).argmax(1)[0]
+        age_pred = self.age_model.predict(img_to_pred).argmax(1)[0]
+        gender = gen_encoder[gender_pred]
+        age = age_encoder[age_pred]
+        prediction = f'Gender: {gender} Age: {age}'
+
         for (x, y, width, height) in detected:
-            cv2.rectangle(image, (x, y), (x + width, y + height), color, thickness=2 )
+            cv2.rectangle(image, (x, y), (x + width, y + height), color, thickness=3)
+            cv2.putText(image, prediction,  (x , y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
+
+
 
     def start_camera(self):
         _, frame = self.vid.read()
@@ -110,16 +129,16 @@ class gad_app(tk.Frame):
         cv2.imwrite(p, self.camera_frame.copy())
 
     def browse_img_file(self):
-        try:
-            filename = filedialog.askopenfilename(initialdir = "/", title = "Select an image", filetypes = (("JPG files", "*.JPG*"), ("all files", "*.*")))
-            img = Image.open(filename).resize((640, 480), Image.ANTIALIAS)
-            self.img = ImageTk.PhotoImage(img)
-            self.image_frame.create_image(0, 0, image = self.img, anchor = tk.NW)
-            
-        except:
-            return
+        filename = filedialog.askopenfilename(initialdir = "/", title = "Select an image", filetypes = (("JPG files", "*.JPG*"), ("all files", "*.*")))
+        self.img = cv2.imdecode(np.fromfile(filename, np.uint8), cv2.IMREAD_UNCHANGED)
+        grayscale_image = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+        detected_faces = self.face_cascade.detectMultiScale(image=grayscale_image, scaleFactor=1.3, minNeighbors=3)
+        self.draw_found_faces(detected_faces, self.img, (0, 255, 0))
+        self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGBA)
+        self.img = PIL.Image.fromarray(self.img).resize((640, 480))
+        self.img = ImageTk.PhotoImage(image=self.img)
+        self.image_frame.create_image(0, 0, image = self.img, anchor = tk.NW)
 
-        
 if __name__ == '__main__':
 
     root = tk.Tk()
